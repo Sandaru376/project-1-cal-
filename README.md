64282476d1d4f9830a7d21b7f168a9d3710ae687
Sandaru Malith Lakshan <135944062+Sandaru376@users.noreply.github.com>
2023-08-08 10:38:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-1-cal
+ using c++
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1,num2,result;
+    char op;
+
+    cout<<"Enter first number="<<endl;
+    cin>>num1;
+    cout<<"select op(+,-,*,/)"<<endl;
+    cin>>op;
+    cout<<"Enter second number="<<endl;
+    cin>>num2;
+
+    switch(op){
+    case '+':
+        result=num1+num2;
+        break;
+     case '-':
+        result=num1-num2;
+        break;
+     case '*':
+        result=num1*num2;
+        break;
+     case '/':
+         if (num2 !=0)
+            result=num1/num2;
+         else{
+             cout<<"invalid"<<endl;
+             return 1;
+         }
+         break;
+     default:
+        cout<<"error"<<endl;
+        return 1;
+
+
+    }
+
+
+    cout<<"result is="<<result;
+
+    return 0;
+}
